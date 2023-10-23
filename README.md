7f7f23c44ad6bf766ccdd0aa18eb349a12543835
MichaelMacleod112 <michael.macleod@inphys.com>
2023-10-23 23:50:40 +0100

    added error handling and updated documentation

--- README.md
+++ README.md
@@ -1,18 +1,30 @@
 # Hangman
-Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
-This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+Hangman is a classic word game where one player thinks of a word, and the other player tries to guess it within a limited number of attempts.
 
+## Description
 
-Installation instructions: none at this time, just run and play!
+This project is an implementation of the Hangman game, where the computer selects a word, and the user attempts to guess it.
 
-Usage instructions: run milestone_5.py to start playing
+## Installation
 
-File structure of the project
+Developed on Python version 3.8.8
+3rd party libraries required for full experience - `nltk`
 
-License information
+## Usage
 
-milestone_2.py - short script to ask for user input and check it is a single alphabetical character
-milestone_3.py - short script built on milestone_2.py to check the user guess against the correct answer
-milestone_4.py - bundles functionality from milestone 3 into a Hangman class
-milestone_5.py - completes hangman classes and creates game functions to run the game multiple times with win and loss conditions
+To play the game, run `milestone_5.py`.
+
+## Project Structure
+
+- `milestone_2.py`: A short script that requests user input and checks if it's a single alphabetical character.
+- `milestone_3.py`: Builds on `milestone_2.py` to validate the user's guess against the correct answer.
+- `milestone_4.py`: Combines functionality from `milestone_3` into a Hangman class.
+- `milestone_5.py`: Completes Hangman classes and game functions. Allows users to choose between a short list of words and a longer word list downloaded from the nltk library. Provides the option to start a new game after completing the previous one. Additionally, it includes various messages to enhance the user experience.
+
+To do: implement p2w cashgrab mechanics
+
+## License
+
+Intellectual property of Michael Macleod 2023
+ 
--- hangman/milestone_5.py
+++ hangman/milestone_5.py
@@ -1,21 +1,61 @@
 from random import choice as random_word_choice
 import string
+import warnings
+
+word_list_short = ["apple", "banana", "orange", "mango", "pear"] # list of possible words
+
+
+# import NLTK library to use a longer list of words to play with
+nltk_import_successful = False  # initialise flag
+try:
+    import nltk
+    nltk.download('words')
+    from nltk.corpus import words
+    word_list_long = words.words() # much longer list of possible words
+    nltk_import_successful = True  # signal nltk has been imported succesfully
+    
+except ImportError as e:
+    warnings.warn("NLTK is not installed. Limited to short possible word list without NLTK.")
+except LookupError as e:
+    warnings.warn("The 'words' corpus data is not found. Limited to short possible word list.")
+
+
 
 class Hangman:
     """ Hangman class to hold all necessary functionality to play a single round of hangman
     """
     
     def __init__(self, num_lives=5):
-        
-        self.word_list = ["apple", "banana", "orange", "mango", "pear"] # list of possible words
+        # thought it more sensible to select word list from within the class rather than pass it in, due to the choice between two lists
+        self.__choose_word_list()
         self.num_lives = num_lives  # number of lives starting at 5
         self.word = random_word_choice(self.word_list)   # choice of word for current round
         self.word_guessed = ['_' for _ in self.word] # empty list to be filled by player guesses corresponding to the answer
         self.num_letters = len(set(self.word))   # number of unique letters left to guess
         self.list_of_guesses = []   # list of guesses by player
         
+        if len(self.word)>5:
+            # increase number of lives if word is long
+            self.num_lives = len(self.word)
+        
         print(f"Word has {len(self.word)} letters and {self.num_letters} unique letters")
     
+    def __choose_word_list(self):
+        """Sets internal word list based on user choice between two options
+        """
+        if nltk_import_successful:
+            print("Would you to play with the long or short list of words? l/s")
+            new_game = input()
+            while new_game.lower() not in ['l', 's']:
+                new_game = input()
+            if new_game == 'l':
+                self.word_list = word_list_long 
+            else:
+                self.word_list = word_list_short
+        else:
+            print("Using short word list")
+            self.word_list = word_list_short
+
     def ask_for_input(self)->chr:
         """Function to take a single character input and check it against the correct word
 
@@ -34,7 +74,7 @@ class Hangman:
                 self.check_guess(guess, self.word)
                 return guess
         
-    def check_guess(self, guess, word):
+    def check_guess(self, guess, word)->None:
         """Check user guess against letters in the correct answer
 
         Args:
@@ -57,7 +97,7 @@ class Hangman:
         """ Gets user input to decide whether or not to start a new round
 
         Returns:
-            bool: starts a new round on True, ends game on False
+            bool : new round started if True, otherwise game exits
         """
         print("Play again? y/n")
         new_game = input()
@@ -66,19 +106,42 @@ class Hangman:
         if new_game == 'y': return True 
         else: return False
     
-def play_game():
+    def is_word_guessed(self)->bool:
+        """Returns bool to check if the word has been correctly guessed
+        """
+        return ''.join(self.word_guessed) == self.word
+        
+        
+def play_game()->bool:
+    """Instantiates hangman class to play a single round of hangman
+
+    Returns:
+        bool: new round of game will start if returning True
+    """
     game_instance = Hangman()
-    print("Game started")
-    while ''.join(game_instance.word_guessed) != game_instance.word:
+    
+    while  not game_instance.is_word_guessed():
         if game_instance.num_lives == 0:
             print("You lost!")
+            #TODO
+            # print("Would you like to purchase more lives? y/n")
+            # if input() == "y":
+            #     print("Please enter your bank details")
+            print(f"The correct word was {game_instance.word}!")
             return Hangman.play_again()
+        
         game_instance.ask_for_input()
-        print(f"The word so far: {game_instance.word_guessed}")
+        
+        if not game_instance.is_word_guessed():
+            print(f"The word so far: {game_instance.word_guessed}")
+            
+    print(f"Congratulations! The correct word was {game_instance.word}!")
     return Hangman.play_again()
 
 
+
 if __name__ == "__main__":
+    print("Welcome to hangman!")
     play = True
     while play == True:
         play = play_game()
